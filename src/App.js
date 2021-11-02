2381e21f15a0ebb493d4b7e564f9edc0b3bf147d
Ricardo Sardón <rst.bcnb@gmail.com>
2021-11-02 13:15:35 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-
+import React from 'react'; 
 import './App.css';
+import Parallax from 'react-rellax'
+
+
+
+//Material UI
+import Button from '@material-ui/core/Button'
+
+//Illustrations 
+import LateAtNight from './components/LateAtNight';
 
 
 function App() {
+
   return (
-   <>
+    <>
+    <div className="app">
+      <div className="left-side">
+        <h1>Demo C4B Rellax 3D Scroll Effect</h1>
+        <p className="paragraph">First sentence that moves rapidly. I love it!</p>
+        <p className="paragraph">This one moves slowly...</p>
+        <p className="paragraph">Again! This sentence is a rapid one!</p>
+        <Button variant="outlined" color="error">
+            More
+        </Button>
+      </div>
 
 
-   </>
 
+      <div className="right-side">
+        <Parallax speed={ -10 }>
+          <LateAtNight/>
+        </Parallax>
+      </div>
+    
+    </div>
+      <div className="green-section">
+      <h2>See you soon!</h2>
+    </div>
+    </>
   );
 }
 
